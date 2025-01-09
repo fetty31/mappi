de3eee2385285dee4b7ae3dc8def9239b39e4717
Oriol <oriol.martinez@vaivelogistics.com>
2025-01-09 12:45:44 +0000

    Defining BaseLocalPlanner controller

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -9,7 +9,7 @@
 
 #include <tf2/utils.h>
 
-NanoMPPIcROS::NanoMPPIcROS()
+NanoMPPIcROS::NanoMPPIcROS() : initialized_(false)
 {
 
 }
@@ -23,7 +23,20 @@ void NanoMPPIcROS::initialize(std::string name,
                                 tf2_ros::Buffer* tf,
                                 costmap_2d::Costmap2DROS* costmap_ros)
 {
-    
+    ros::NodeHandle private_nh("~/" + name);
+    tf_ = tf;
+
+    costmap_ros_ = costmap_ros;
+
+    /* To-Do:
+        - fill config::Predictor obj
+        - initialize nano_mppic_ obj
+            . probably must change std::shared_ptr into boost::shared_ptr
+            . make macro for changing std/boost 
+    */
+    // nano_mppic_.configure()
+
+    initialized_ = true;
 }
 
 void NanoMPPIcROS::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
@@ -43,7 +56,7 @@ bool NanoMPPIcROS::isGoalReached()
 
 bool NanoMPPIcROS::is_initialized()
 {
-
+ return initialized_;
 }
 
 PLUGINLIB_EXPORT_CLASS(NanoMPPIcROS, nav_core::BaseLocalPlanner)
