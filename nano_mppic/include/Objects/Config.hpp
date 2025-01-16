615c6c3c32dcfc81948736b82795ddf5ed48b2a6
Oriol <oriol.martinez@vaivelogistics.com>
2025-01-16 12:52:35 +0000

    Config now printing out for debug purposes

--- nano_mppic/include/Objects/Config.hpp
+++ nano_mppic/include/Objects/Config.hpp
@@ -57,7 +57,23 @@ struct MPPIc
 
 void MPPIc::print_out(){
     std::cout << "//////////////// MPPIc::Configuration ////////////////\n";
-    std::cout << "model_dt: " << model_dt 
+    std::cout << "num_iters: " << settings.num_iters << std::endl;
+    std::cout << "num_retry: " << settings.num_retry << std::endl;
+    std::cout << "batch_size: " << noise.batch_size << std::endl;
+    std::cout << "time_steps: " << noise.time_steps << std::endl;
+    std::cout << "offset: " << settings.offset << std::endl;
+    std::cout << "model_dt: " << model_dt << std::endl;
+    std::cout << "temperature: " << temperature  << std::endl;
+    std::cout << "gamma: " << gamma << std::endl;
+    std::cout << "motion_model: " << settings.motion_model << std::endl;
+    if (settings.motion_model=="Ackermann")
+        std::cout << "ackermann.min_r: " << ackermann.min_r << std::endl;
+    std::cout << "noise.std_vx: " << noise.std_vx << std::endl;
+    std::cout << "noise.std_vy: " << noise.std_vy << std::endl;
+    std::cout << "noise.std_wz: " << noise.std_wz << std::endl;
+    std::cout << "bounds.max_vx: " << bounds.max_vx << "   bounds.min_vx: " << bounds.min_vx << std::endl;
+    std::cout << "bounds.max_vy: " << bounds.max_vy << "   bounds.min_vy: " << bounds.min_vy << std::endl;
+    std::cout << "bounds.max_wz: " << bounds.max_wz << "   bounds.min_wz: " << bounds.min_wz 
     << "//////////////////////////////////////////////////////\n" <<
     std::endl;
 }
