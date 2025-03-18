bfb1eece45a95febca111ea5065dfe6d354c0a69
Oriol <oriol.martinez@vaivelogistics.com>
2025-03-18 14:14:10 +0000

    NavFn wrapper with lateral costs + re-compute flag

--- include/NavFnWrapper.hpp
+++ include/NavFnWrapper.hpp
@@ -46,6 +46,9 @@ class NavFnWrapper {
         bool validPointPotential(const geometry_msgs::Point& world_point, double tolerance);
 
         double getTolerance();
+
+        bool timeToPlan(std::vector<geometry_msgs::PoseStamped>& plan,
+                        geometry_msgs::PoseStamped& goal);
   
     protected:
         mappi::shared_ptr<costmap_2d::Costmap2DROS> costmap_ros_ptr_;
@@ -53,6 +56,9 @@ class NavFnWrapper {
         mappi::shared_ptr<navfn::NavFn> planner_;
 
         bool allow_unknown_;
+
+        std::vector<geometry_msgs::PoseStamped> plan_ros_;
+        geometry_msgs::PoseStamped last_goal_;
 
     private:
         inline double sq_distance(const geometry_msgs::PoseStamped& p1, const geometry_msgs::PoseStamped& p2){
@@ -62,9 +68,18 @@ class NavFnWrapper {
         }
 
         geometry_msgs::PoseStamped chooseGoal(mappi::objects::Path&);
+
         geometry_msgs::PoseStamped chooseStart(mappi::objects::Odometry2d&, int mode=0);
+
+        void fillRobotFootprint(mappi::objects::Odometry2d& odom);
+
         void mapToWorld(double mx, double my, double& wx, double& wy);
-        void clearRobotCell(const geometry_msgs::PoseStamped& global_pose, unsigned int mx, unsigned int my);
+
+        void clearRobotCell(unsigned int mx, unsigned int my);
+
+        void fillRobotCell(unsigned int mx, unsigned int my);
+
+        bool isInCollision(unsigned char cost);
 
         double default_tolerance_;
         double start_shift_;
@@ -86,11 +101,14 @@ NavFnWrapper::~NavFnWrapper() { delete costmap_; }
 
 void NavFnWrapper::configure(std::string name, mappi::shared_ptr<costmap_2d::Costmap2DROS>& costmap_ros)
 {
+    costmap_ = new costmap_2d::Costmap2D(); // allocate memory
+
     costmap_ros_ptr_ = costmap_ros;
-    costmap_ = costmap_ros->getCostmap();
     global_frame_ = costmap_ros->getGlobalFrameID();
     planner_ = mappi::shared_ptr
-                        <navfn::NavFn>(new navfn::NavFn(costmap_->getSizeInCellsX(), costmap_->getSizeInCellsY()) );
+                        <navfn::NavFn>(new navfn::NavFn(costmap_ros->getCostmap()->getSizeInCellsX(), 
+                                                        costmap_ros->getCostmap()->getSizeInCellsY()) 
+                                        );
 
     ros::NodeHandle private_nh("~/" + name);
 
@@ -104,25 +122,46 @@ bool NavFnWrapper::getPlan(mappi::objects::Odometry2d odom,
                             mappi::objects::Path& out_plan
                             )
 {
+    costmap_2d::Costmap2D costmap_cpy = *costmap_ros_ptr_->getCostmap(); // define cpy of costmap (because we'll fill some cells before planning)
+    *costmap_ = costmap_cpy;
+
     geometry_msgs::PoseStamped start;
     start = chooseStart(odom, 1); // mode=1 (shift start point along positive x-axis)
 
     geometry_msgs::PoseStamped goal;
     goal = chooseGoal(goals);
 
-    std::vector<geometry_msgs::PoseStamped> plan_ros;
-    if(this->makePlan(start, goal, plan_ros)){
-        mappi::ros_utils::ros2mppic(plan_ros, out_plan);
+    // Decide if we really need to re-compute trajectory
+    if(not timeToPlan(plan_ros_, goal)){
+        mappi::ros_utils::ros2mppic(plan_ros_, out_plan);
+        return true; // sending last computed plan
+    }
+    last_goal_ = goal;
+
+    unsigned int mx, my;
+    if(!costmap_->worldToMap(start.pose.position.x, start.pose.position.y, mx, my)){
+        ROS_WARN_THROTTLE(1.0, "The robot's start position is off the global costmap. Planning will always fail, are you sure the robot has been properly localized?");
+        return false;
+    }
+
+    // Clear the starting cell within the costmap because we know it can't be an obstacle
+    clearRobotCell(mx, my);
+
+    // (optional) Add virtual costs around the robot (Ona needs to avoid perpendicular trajectories)
+    fillRobotFootprint(odom);
+
+    if(this->makePlan(start, goal, plan_ros_)){
+        mappi::ros_utils::ros2mppic(plan_ros_, out_plan);
         return true;
     }
-    else if(this->makePlan(chooseStart(odom, 2), goal, plan_ros)){ // mode=2 (shift start point along negative x-axis)
+    else if(this->makePlan(chooseStart(odom, 2), goal, plan_ros_)){ // mode=2 (shift start point along negative x-axis)
         ROS_WARN("NAVFN_WRAPPER:: path found with start point shifting along negative x-axis!");
-        mappi::ros_utils::ros2mppic(plan_ros, out_plan);
+        mappi::ros_utils::ros2mppic(plan_ros_, out_plan);
         return true;
     }
-    else if(this->makePlan(chooseStart(odom), goal, plan_ros)){ // mode=0 (do not shift start point)
+    else if(this->makePlan(chooseStart(odom), goal, plan_ros_)){ // mode=0 (do not shift start point)
         ROS_WARN("NAVFN_WRAPPER:: path found with no start point shifting!");
-        mappi::ros_utils::ros2mppic(plan_ros, out_plan);
+        mappi::ros_utils::ros2mppic(plan_ros_, out_plan);
         return true;
     }
     else
@@ -171,104 +210,13 @@ geometry_msgs::PoseStamped NavFnWrapper::chooseStart(mappi::objects::Odometry2d&
     else                // no shifting
         x = 0.0f;
 
-    chosen_start.pose.position.x += x*cos(odom.yaw) + y*sin(odom.yaw);
-    chosen_start.pose.position.y += -x*sin(odom.yaw) + y*cos(odom.yaw);
+    // chosen_start.pose.position.x += x*cos(odom.yaw) + y*sin(odom.yaw);
+    // chosen_start.pose.position.y += -x*sin(odom.yaw) + y*cos(odom.yaw);
+
+    chosen_start.pose.position.x += x*cos(odom.yaw) - y*sin(odom.yaw);
+    chosen_start.pose.position.y += x*sin(odom.yaw) + y*cos(odom.yaw);
 
     return chosen_start;
-}
-
-bool NavFnWrapper::validPointPotential(const geometry_msgs::Point& world_point)
-{
-    return validPointPotential(world_point, default_tolerance_);
-}
-
-bool NavFnWrapper::validPointPotential(const geometry_msgs::Point& world_point, double tolerance)
-{
-    if(not costmap_ros_ptr_){
-        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
-        return false;
-    }
-
-    double resolution = costmap_->getResolution();
-    geometry_msgs::Point p;
-    p = world_point;
-
-    p.y = world_point.y - tolerance;
-
-    while(p.y <= world_point.y + tolerance){
-        p.x = world_point.x - tolerance;
-        while(p.x <= world_point.x + tolerance){
-            double potential = getPointPotential(p);
-            if(potential < /*navfn::*/POT_HIGH){
-            return true;
-            }
-            p.x += resolution;
-        }
-        p.y += resolution;
-    }
-
-    return false;
-}
-
-double NavFnWrapper::getPointPotential(const geometry_msgs::Point& world_point)
-{
-    if(not costmap_ros_ptr_){
-        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
-        return -1.0;
-    }
-
-    unsigned int mx, my;
-    if(!costmap_->worldToMap(world_point.x, world_point.y, mx, my))
-        return std::numeric_limits<double>::max();
-
-    unsigned int index = my * planner_->nx + mx;
-    return planner_->potarr[index];
-}
-
-bool NavFnWrapper::computePotential(const geometry_msgs::Point& world_point)
-{
-    if(not costmap_ros_ptr_){
-        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
-        return false;
-    }
-
-    //make sure to resize the underlying array that Navfn uses
-    planner_->setNavArr(costmap_->getSizeInCellsX(), costmap_->getSizeInCellsY());
-    planner_->setCostmap(costmap_->getCharMap(), true, allow_unknown_);
-
-    unsigned int mx, my;
-    if(!costmap_->worldToMap(world_point.x, world_point.y, mx, my))
-        return false;
-
-    int map_start[2];
-    map_start[0] = 0;
-    map_start[1] = 0;
-
-    int map_goal[2];
-    map_goal[0] = mx;
-    map_goal[1] = my;
-
-    planner_->setStart(map_start);
-    planner_->setGoal(map_goal);
-
-    return planner_->calcNavFnDijkstra();
-}
-
-void NavFnWrapper::clearRobotCell(const geometry_msgs::PoseStamped& global_pose, unsigned int mx, unsigned int my)
-{
-    if(not costmap_ros_ptr_){
-        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
-        return;
-    }
-
-    //set the associated costs in the cost map to be free
-    costmap_->setCost(mx, my, costmap_2d::FREE_SPACE);
-}
-
-void NavFnWrapper::mapToWorld(double mx, double my, double& wx, double& wy)
-{
-    wx = costmap_->getOriginX() + mx * costmap_->getResolution();
-    wy = costmap_->getOriginY() + my * costmap_->getResolution();
 }
 
 bool NavFnWrapper::makePlan(const geometry_msgs::PoseStamped& start, 
@@ -308,13 +256,8 @@ bool NavFnWrapper::makePlan(const geometry_msgs::PoseStamped& start,
     double wy = start.pose.position.y;
 
     unsigned int mx, my;
-    if(!costmap_->worldToMap(wx, wy, mx, my)){
-        ROS_WARN_THROTTLE(1.0, "The robot's start position is off the global costmap. Planning will always fail, are you sure the robot has been properly localized?");
+    if(!costmap_->worldToMap(wx, wy, mx, my))
         return false;
-    }
-
-    //clear the starting cell within the costmap because we know it can't be an obstacle
-    clearRobotCell(start, mx, my);
 
     //make sure to resize the underlying array that Navfn uses
     planner_->setNavArr(costmap_->getSizeInCellsX(), costmap_->getSizeInCellsY());
@@ -448,9 +391,201 @@ bool NavFnWrapper::getPlanFromPotential(const geometry_msgs::PoseStamped& goal, 
     return !plan.empty();
 }
 
+bool NavFnWrapper::validPointPotential(const geometry_msgs::Point& world_point)
+{
+    return validPointPotential(world_point, default_tolerance_);
+}
+
+bool NavFnWrapper::validPointPotential(const geometry_msgs::Point& world_point, double tolerance)
+{
+    if(not costmap_ros_ptr_){
+        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
+        return false;
+    }
+
+    double resolution = costmap_->getResolution();
+    geometry_msgs::Point p;
+    p = world_point;
+
+    p.y = world_point.y - tolerance;
+
+    while(p.y <= world_point.y + tolerance){
+        p.x = world_point.x - tolerance;
+        while(p.x <= world_point.x + tolerance){
+            double potential = getPointPotential(p);
+            if(potential < /*navfn::*/POT_HIGH){
+            return true;
+            }
+            p.x += resolution;
+        }
+        p.y += resolution;
+    }
+
+    return false;
+}
+
+double NavFnWrapper::getPointPotential(const geometry_msgs::Point& world_point)
+{
+    if(not costmap_ros_ptr_){
+        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
+        return -1.0;
+    }
+
+    unsigned int mx, my;
+    if(!costmap_->worldToMap(world_point.x, world_point.y, mx, my))
+        return std::numeric_limits<double>::max();
+
+    unsigned int index = my * planner_->nx + mx;
+    return planner_->potarr[index];
+}
+
+bool NavFnWrapper::computePotential(const geometry_msgs::Point& world_point)
+{
+    if(not costmap_ros_ptr_){
+        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
+        return false;
+    }
+
+    //make sure to resize the underlying array that Navfn uses
+    planner_->setNavArr(costmap_->getSizeInCellsX(), costmap_->getSizeInCellsY());
+    planner_->setCostmap(costmap_->getCharMap(), true, allow_unknown_);
+
+    unsigned int mx, my;
+    if(!costmap_->worldToMap(world_point.x, world_point.y, mx, my))
+        return false;
+
+    int map_start[2];
+    map_start[0] = 0;
+    map_start[1] = 0;
+
+    int map_goal[2];
+    map_goal[0] = mx;
+    map_goal[1] = my;
+
+    planner_->setStart(map_start);
+    planner_->setGoal(map_goal);
+
+    return planner_->calcNavFnDijkstra();
+}
+
+void NavFnWrapper::fillRobotFootprint(mappi::objects::Odometry2d& odom)
+{
+    std::vector<geometry_msgs::Point> footprint = costmap_ros_ptr_->getRobotFootprint();
+    // NOTE: here we are assuming the footprint is ordered as in footprint param
+    //      - in Ona's case: [FrontLeft, FrontRight, RearRight, RearLeft] (see vaive_rosnav/config/costmap_common_params.yaml)
+
+    if( footprint.size() < 4 ){
+        ROS_WARN("NAVFN_WRAPPER::Robot's footprint is not a rectangle, no virtual costs will be added");
+        return;
+    }
+
+    // fill robot's laterals with virtual costs
+    unsigned int mx, my;
+    double dx = costmap_->getResolution();
+    double x, y;
+
+    // get number of cells on the laterals
+    int n_cells = std::ceil( (std::fabs(footprint[0].x) + std::fabs(footprint[2].x))/costmap_->getResolution() );
+
+    // left side
+    y = footprint[0].y; // FrontLeft y coord
+    x = footprint[0].x; // FrontLeft x coord
+    for(int i=0; i < n_cells; i++){
+        x -= dx;
+        double x_odom = odom.x + x*cos(odom.yaw) + y*sin(odom.yaw);
+        double y_odom = odom.y + -x*sin(odom.yaw) + y*cos(odom.yaw);
+
+        costmap_->worldToMap(x_odom, y_odom, mx, my);
+
+        fillRobotCell(mx, my);
+    }
+
+    // right side
+    y = footprint[1].y; // FrontRight y coord
+    x = footprint[1].x; // FrontRight x coord
+    for(int i=0; i < n_cells; i++){
+        x -= dx;
+        double x_odom = odom.x + x*cos(odom.yaw) + y*sin(odom.yaw);
+        double y_odom = odom.y + -x*sin(odom.yaw) + y*cos(odom.yaw);
+
+        costmap_->worldToMap(x_odom, y_odom, mx, my);
+
+        fillRobotCell(mx, my);
+    }
+
+}
+
+void NavFnWrapper::clearRobotCell(unsigned int mx, unsigned int my)
+{
+    if(not costmap_ros_ptr_){
+        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
+        return;
+    }
+
+    //set the associated costs in the cost map to be free
+    costmap_->setCost(mx, my, costmap_2d::FREE_SPACE);
+}
+
+void NavFnWrapper::fillRobotCell(unsigned int mx, unsigned int my)
+{
+    if(not costmap_ros_ptr_){
+        ROS_ERROR("NAVFN_WRAPPER::This planner has not been initialized yet, but it is being used, please call initialize() before use");
+        return;
+    }
+
+    //set the associated costs in the cost map to be lethal
+    costmap_->setCost(mx, my, costmap_2d::LETHAL_OBSTACLE);
+}
+
+void NavFnWrapper::mapToWorld(double mx, double my, double& wx, double& wy)
+{
+    wx = costmap_->getOriginX() + mx * costmap_->getResolution();
+    wy = costmap_->getOriginY() + my * costmap_->getResolution();
+}
+
 double NavFnWrapper::getTolerance()
 {
     return default_tolerance_;
+}
+
+bool NavFnWrapper::timeToPlan(std::vector<geometry_msgs::PoseStamped>& plan, 
+                                geometry_msgs::PoseStamped& goal )
+{
+    if(plan.size() < 1)
+        return true;
+
+    if( (goal.pose.position.x != last_goal_.pose.position.x) ||
+        (goal.pose.position.y != last_goal_.pose.position.y) )
+        return true;
+
+    unsigned int mx, my;
+    for(auto point : plan){
+        if(not costmap_->worldToMap(point.pose.position.x, point.pose.position.y, mx, my))
+            return true;
+        if(isInCollision(costmap_->getCost(mx, my)))
+            return true; 
+    }
+
+    return false; // do not re-compute trajectory
+}
+
+bool NavFnWrapper::isInCollision(unsigned char cost)
+{
+    bool is_tracking_unkown = 
+        costmap_ros_ptr_->getLayeredCostmap()->isTrackingUnknown();
+
+    switch(cost) {
+        case(costmap_2d::LETHAL_OBSTACLE):
+            return true;
+        case(costmap_2d::INSCRIBED_INFLATED_OBSTACLE):
+            return true;
+        case(costmap_2d::NO_INFORMATION):
+            return is_tracking_unkown ? false : true;
+        case(costmap_2d::FREE_SPACE):
+            return false;
+        default:
+            return false;
+    }
 }
 
 #endif
